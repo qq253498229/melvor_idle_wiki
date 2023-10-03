5c69b1e0617fa7a6120d9b1fd3f581b6aef55812
王斌 <253498229@qq.com>
2023-10-04 02:50:27 +0800

    feat: 更新页面

---
+++ script/translate.ts
@@ -0,0 +1,45 @@
+import * as fs from 'fs-extra';
+
+const configJson = './translate.json';
+const sourceDirectory = '../source/bak'; // 原始文件目录
+const targetDirectory = '../source/页面'; // 输出文件目录
+
+function replaceText(config: any, type: string, fileText: string) {
+  let newFileType = fileText;
+  if ('regex' === type) {
+    for (let regex of config.regex) {
+      let split = regex.split('%%%');
+      for (let i = 1; i < split.length; i++) {
+        newFileType = newFileType.replace(new RegExp(split[i], 'g'), split[0]);
+      }
+    }
+  }
+  if ('character' === type) {
+    for (let regex of config.character) {
+      let split = regex.split('%%%');
+      for (let i = 1; i < split.length; i++) {
+        newFileType = newFileType.replaceAll(split[i], split[0]);
+      }
+    }
+  }
+  return newFileType;
+}
+
+function translate() {
+  try {
+    const jsonObject = fs.readJsonSync(configJson);
+    const files = fs.readdirSync(sourceDirectory);
+    for (let file of files) {
+      if (!file.endsWith('.txt')) continue;
+      // fs.read;
+      const fileText = fs.readFileSync(`${sourceDirectory}/${file}`, 'utf8');
+      let newFileText = replaceText(jsonObject, 'regex', fileText);
+      newFileText = replaceText(jsonObject, 'character', newFileText);
+      fs.outputFileSync(`${targetDirectory}/${file}`, newFileText, 'utf8');
+    }
+  } catch (e) {
+    console.error('发生错误:', e);
+  }
+}
+
+translate();
